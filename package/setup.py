1460f488405b6248bccfa851ff2f492878b657cf
SilverGuo <guoyuhan819@gmail.com>
2017-09-25 16:06:26 +0200

    fix setup

--- package/setup.py
+++ package/setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
-REQUIREMENTS = ['pip>=9.0.0, <10.0.0', ]
+REQUIREMENTS = ['pip>=9.0.0, <10.0.0',
+                'tensorflow>=1.3.0, tensorflow<2.0.0', ]
 
 setup(
     name='koroker',
     version='0.0.1',
-    description='Python package for sequence labeling',
-    url='https://github.com/SilverGuo/koroker',
+    description='python package for sequence labeling',
+    url='https://gitlab.com/SilverGuo/koroker',
     author='Yuhan',
     author_email='guoyuhan819@gmail.com',
     license='MIT',
